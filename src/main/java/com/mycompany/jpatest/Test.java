30d112e002b69e896ddcb1546bff99ef301685f5
RR <rene.roesch.99mail.com#>
2020-12-14 07:47:40 +0100

    reduce log level

--- src/main/java/com/mycompany/jpatest/Test.java
+++ src/main/java/com/mycompany/jpatest/Test.java
@@ -1,20 +1,24 @@
 package com.mycompany.jpatest;
 
+import java.util.logging.*;
+
 /**
  * @author Rene
  */
 public class Test {
 
 	public static void main(String[] args) {
-		CustomerDBMapper dao = new CustomerDBMapper();
+            LogManager.getLogManager().getLogger("").setLevel(Level.SEVERE);
+            
+            CustomerDBMapper dao = new CustomerDBMapper();
 
-		// dao.addCustomer(1, "Sue", "Smith");
-		// dao.addCustomer(2, "Sam", "Smith");
-		// dao.addCustomer(3, "Sid", "Smith");
-		// dao.addCustomer(4, "Sally", "Smith");
-		// dao.changeFName(4, "Mark");
-		// dao.deleteCustomer(3);
-		dao.getCustomers();
+            // dao.addCustomer(1, "Sue", "Smith");
+            // dao.addCustomer(2, "Sam", "Smith");
+            // dao.addCustomer(3, "Sid", "Smith");
+            // dao.addCustomer(4, "Sally", "Smith");
+            // dao.changeFName(4, "Mark");
+            // dao.deleteCustomer(3);
+            dao.getCustomers();
 	}
 
 }
