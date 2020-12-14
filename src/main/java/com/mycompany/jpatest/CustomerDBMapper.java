dbb95ce4246b03906ed49ea6761902f58c2ddf12
RR <rene.roesch.99mail.com#>
2020-12-14 06:54:14 +0100

    add Customer constructor

--- src/main/java/com/mycompany/jpatest/CustomerDBMapper.java
+++ src/main/java/com/mycompany/jpatest/CustomerDBMapper.java
@@ -8,25 +8,23 @@ import javax.persistence.*;
  */
 public class CustomerDBMapper {
     private static final EntityManagerFactory ENTITY_MANAGER_FACTORY = Persistence
-        .createEntityManagerFactory("JPATest");
-    
-    public void addCustomer(int id, String fname, String lname) {
-        // The EntityManager class allows operations such as create, read, update, delete
+            .createEntityManagerFactory("JPATest");
+
+    public void addCustomer(int id, String fName, String lName) {
+        // The EntityManager class allows operations such as create, read, update,
+        // delete
         EntityManager em = ENTITY_MANAGER_FACTORY.createEntityManager();
         // Used to issue transactions on the EntityManager
         EntityTransaction et = null;
- 
+
         try {
             // Get transaction and start
             et = em.getTransaction();
             et.begin();
- 
+
             // Create and set values for new customer
-            Customer cust = new Customer();
-            cust.setID(id);
-            cust.setFName(fname);
-            cust.setLName(lname);
- 
+            Customer cust = new Customer(id, fName, lName);
+
             // Save the customer object
             em.persist(cust);
             et.commit();
@@ -41,71 +39,67 @@ public class CustomerDBMapper {
             em.close();
         }
     }
-    
+
     public void getCustomer(int id) {
-    	EntityManager em = ENTITY_MANAGER_FACTORY.createEntityManager();
-    	
-    	// the lowercase c refers to the object
-    	// :custID is a parameterized query thats value is set below
-    	String query = "SELECT c FROM Customer c WHERE c.id = :custID";
-    	
-    	// Issue the query and get a matching Customer
-    	TypedQuery<Customer> tq = em.createQuery(query, Customer.class);
-    	tq.setParameter("custID", id);
-    	
-    	Customer cust;
-    	try {
+        EntityManager em = ENTITY_MANAGER_FACTORY.createEntityManager();
+
+        // the lowercase c refers to the object
+        // :custID is a parameterized query thats value is set below
+        String query = "SELECT c FROM Customer c WHERE c.id = :custID";
+
+        // Issue the query and get a matching Customer
+        TypedQuery<Customer> tq = em.createQuery(query, Customer.class);
+        tq.setParameter("custID", id);
+
+        Customer cust;
+        try {
             // Get matching customer object and output
             cust = tq.getSingleResult();
-            System.out.println(cust.getFName() + " " + cust.getLName());
-    	}
-    	catch(NoResultException ex) {
+            System.out.println(cust);
+        } catch (NoResultException ex) {
             System.out.println(ex.getMessage());
-    	}
-    	finally {
+        } finally {
             em.close();
-    	}
+        }
     }
-    
+
     public void getCustomers() {
-    	EntityManager em = ENTITY_MANAGER_FACTORY.createEntityManager();
-    	
-    	// the lowercase c refers to the object
-    	// :custID is a parameterized query thats value is set below
-    	String strQuery = "SELECT c FROM Customer c WHERE c.id IS NOT NULL";
-    	
-    	// Issue the query and get a matching Customer
-    	TypedQuery<Customer> tq = em.createQuery(strQuery, Customer.class);
-    	List<Customer> custs;
-    	try {
-    		// Get matching customer object and output
-    		custs = tq.getResultList();
-    		custs.forEach(cust->System.out.println(cust.getFName() + " " + cust.getLName()));
-                System.out.println("");
-    	}
-    	catch(NoResultException ex) {
+        EntityManager em = ENTITY_MANAGER_FACTORY.createEntityManager();
+
+        // the lowercase c refers to the object
+        // :custID is a parameterized query thats value is set below
+        String strQuery = "SELECT c FROM Customer c WHERE c.id IS NOT NULL";
+
+        // Issue the query and get a matching Customer
+        TypedQuery<Customer> tq = em.createQuery(strQuery, Customer.class);
+        List<Customer> custs;
+        try {
+            // Get matching customer object and output
+            custs = tq.getResultList();
+            custs.forEach(cust -> System.out.println(cust));
+            System.out.println("");
+        } catch (NoResultException ex) {
             System.out.println(ex.getMessage());
-    	}
-    	finally {
-    		em.close();
-    	}
+        } finally {
+            em.close();
+        }
     }
-    
+
     public void changeFName(int id, String fname) {
         EntityManager em = ENTITY_MANAGER_FACTORY.createEntityManager();
         EntityTransaction et = null;
-        
-    	Customer cust;
- 
+
+        Customer cust;
+
         try {
             // Get transaction and start
             et = em.getTransaction();
             et.begin();
- 
+
             // Find customer and make changes
             cust = em.find(Customer.class, id);
             cust.setFName(fname);
- 
+
             // Save the customer object
             em.persist(cust);
             et.commit();
@@ -120,12 +114,12 @@ public class CustomerDBMapper {
             em.close();
         }
     }
-    
+
     public void deleteCustomer(int id) {
-    	EntityManager em = ENTITY_MANAGER_FACTORY.createEntityManager();
+        EntityManager em = ENTITY_MANAGER_FACTORY.createEntityManager();
         EntityTransaction et = null;
         Customer cust;
- 
+
         try {
             et = em.getTransaction();
             et.begin();
