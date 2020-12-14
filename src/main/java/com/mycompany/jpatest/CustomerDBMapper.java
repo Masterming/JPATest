b2ea401e404d1e745950b08c591c0ec7e579804b
RR <rene.roesch.99mail.com#>
2020-12-14 06:42:35 +0100

    setup

---
+++ src/main/java/com/mycompany/jpatest/CustomerDBMapper.java
@@ -0,0 +1,146 @@
+package com.mycompany.jpatest;
+
+import java.util.List;
+import javax.persistence.*;
+
+/**
+ * @author Rene
+ */
+public class CustomerDBMapper {
+    private static final EntityManagerFactory ENTITY_MANAGER_FACTORY = Persistence
+        .createEntityManagerFactory("JPATest");
+    
+    public void addCustomer(int id, String fname, String lname) {
+        // The EntityManager class allows operations such as create, read, update, delete
+        EntityManager em = ENTITY_MANAGER_FACTORY.createEntityManager();
+        // Used to issue transactions on the EntityManager
+        EntityTransaction et = null;
+ 
+        try {
+            // Get transaction and start
+            et = em.getTransaction();
+            et.begin();
+ 
+            // Create and set values for new customer
+            Customer cust = new Customer();
+            cust.setID(id);
+            cust.setFName(fname);
+            cust.setLName(lname);
+ 
+            // Save the customer object
+            em.persist(cust);
+            et.commit();
+        } catch (Exception ex) {
+            // If there is an exception rollback changes
+            if (et != null) {
+                et.rollback();
+            }
+            System.out.println(ex.getMessage());
+        } finally {
+            // Close EntityManager
+            em.close();
+        }
+    }
+    
+    public void getCustomer(int id) {
+    	EntityManager em = ENTITY_MANAGER_FACTORY.createEntityManager();
+    	
+    	// the lowercase c refers to the object
+    	// :custID is a parameterized query thats value is set below
+    	String query = "SELECT c FROM Customer c WHERE c.id = :custID";
+    	
+    	// Issue the query and get a matching Customer
+    	TypedQuery<Customer> tq = em.createQuery(query, Customer.class);
+    	tq.setParameter("custID", id);
+    	
+    	Customer cust;
+    	try {
+            // Get matching customer object and output
+            cust = tq.getSingleResult();
+            System.out.println(cust.getFName() + " " + cust.getLName());
+    	}
+    	catch(NoResultException ex) {
+            System.out.println(ex.getMessage());
+    	}
+    	finally {
+            em.close();
+    	}
+    }
+    
+    public void getCustomers() {
+    	EntityManager em = ENTITY_MANAGER_FACTORY.createEntityManager();
+    	
+    	// the lowercase c refers to the object
+    	// :custID is a parameterized query thats value is set below
+    	String strQuery = "SELECT c FROM Customer c WHERE c.id IS NOT NULL";
+    	
+    	// Issue the query and get a matching Customer
+    	TypedQuery<Customer> tq = em.createQuery(strQuery, Customer.class);
+    	List<Customer> custs;
+    	try {
+    		// Get matching customer object and output
+    		custs = tq.getResultList();
+    		custs.forEach(cust->System.out.println(cust.getFName() + " " + cust.getLName()));
+                System.out.println("");
+    	}
+    	catch(NoResultException ex) {
+            System.out.println(ex.getMessage());
+    	}
+    	finally {
+    		em.close();
+    	}
+    }
+    
+    public void changeFName(int id, String fname) {
+        EntityManager em = ENTITY_MANAGER_FACTORY.createEntityManager();
+        EntityTransaction et = null;
+        
+    	Customer cust;
+ 
+        try {
+            // Get transaction and start
+            et = em.getTransaction();
+            et.begin();
+ 
+            // Find customer and make changes
+            cust = em.find(Customer.class, id);
+            cust.setFName(fname);
+ 
+            // Save the customer object
+            em.persist(cust);
+            et.commit();
+        } catch (Exception ex) {
+            // If there is an exception rollback changes
+            if (et != null) {
+                et.rollback();
+            }
+            System.out.println(ex.getMessage());
+        } finally {
+            // Close EntityManager
+            em.close();
+        }
+    }
+    
+    public void deleteCustomer(int id) {
+    	EntityManager em = ENTITY_MANAGER_FACTORY.createEntityManager();
+        EntityTransaction et = null;
+        Customer cust;
+ 
+        try {
+            et = em.getTransaction();
+            et.begin();
+            cust = em.find(Customer.class, id);
+            em.remove(cust);
+            et.commit();
+        } catch (Exception ex) {
+            // If there is an exception rollback changes
+            if (et != null) {
+                et.rollback();
+            }
+            System.out.println(ex.getMessage());
+        } finally {
+            // Close EntityManager
+            em.close();
+        }
+    }
+}
---
+++ src/main/java/com/mycompany/jpatest/Test.java
@@ -0,0 +1,22 @@
+package com.mycompany.jpatest;
+
+/**
+ * @author Rene
+ */
+public class Test {
+    
+    public static void main(String[] args) {
+        CustomerDBMapper dao = new CustomerDBMapper();
+        
+//    	dao.addCustomer(1, "Sue", "Smith");
+//	dao.addCustomer(2, "Sam", "Smith");
+//	dao.addCustomer(3, "Sid", "Smith");
+//	dao.addCustomer(4, "Sally", "Smith");
+	dao.getCustomers();
+        
+	dao.changeFName(4, "Mark");
+	dao.deleteCustomer(3); 
+	dao.getCustomers();
+    }
+
+}
